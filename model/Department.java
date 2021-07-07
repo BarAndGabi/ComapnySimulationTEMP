9239ef119cfaeeb7d1e91c219cf8454851420edb
barmoshe <1barmoshe1@gmail.com>
2021-07-07 21:30:12 +0300

    department configuration - not finish

--- model/Department.java
+++ model/Department.java
@@ -7,6 +7,22 @@ public class Department implements syncAble, choosePreference {
 	private ArrayList<Role> roles;
 	private boolean sync;
 
+	public Department(boolean sync) {
+		setRoles(this.roles);
+		setEmployeesByRoles(this.employeesByRoles);
+
+	}
+
+	private void setEmployeesByRoles(ArrayList<ArrayList<Employee>> employeesByRoles2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setRoles(ArrayList<Role> roles) {
+		// TODO Auto-generated method stub
+
+	}
+
 	@Override
 	public void syncObjectPrefrence(Preference p) {
 		// TODO Auto-generated method stub

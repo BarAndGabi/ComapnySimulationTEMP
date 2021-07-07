b46ff0f1f6e3f84fd07b8c58579b1990bb8268e3
barmoshe <1barmoshe1@gmail.com>
2021-07-07 12:42:48 +0300

    Create Department.java

---
+++ listeners/Department.java
@@ -0,0 +1,9 @@
+package application.listeners;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Department {
+	private ArrayList<Set<Employee>> employees;
+	private Set<Role> roles;
+}

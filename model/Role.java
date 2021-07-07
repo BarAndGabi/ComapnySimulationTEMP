0625ec6148ef4fc5cd9523caa8ea7100be0a1836
barmoshe <1barmoshe1@gmail.com>
2021-07-07 14:09:07 +0300

    added list of employees to role

--- model/Role.java
+++ model/Role.java
@@ -1,8 +1,11 @@
 package application.model;
 
+import java.util.ArrayList;
+
 public class Role {
 
 	private double ProfitPerHour;
 	private String jobTitle;
+	private ArrayList<Employee> EmployeesWithCurrentRole;
 
 }

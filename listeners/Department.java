8fd222ba94dd72551a8656f862214cf9eae0d5e4
barmoshe <1barmoshe1@gmail.com>
2021-07-07 12:43:13 +0300

    Delete Department.java

--- listeners/Department.java
+++
@@ -1,9 +0,0 @@
-package application.listeners;
-
-import java.util.ArrayList;
-import java.util.Set;
-
-public class Department {
-	private ArrayList<Set<Employee>> employees;
-	private Set<Role> roles;
-}

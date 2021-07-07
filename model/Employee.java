2b9b13d9b4b2ba7a537c6d6f078540679e89428f
barmoshe <1barmoshe1@gmail.com>
2021-07-07 14:07:05 +0300

    .

--- model/Employee.java
+++ model/Employee.java
@@ -3,7 +3,7 @@ package application.model;
 public class Employee extends Person {
 	private int employeeId;
 	private static int idSerialCounter = 1000;
-
+	private Preference preference ;
 	public Employee(String name, int yearOfBirth) throws Exception {
 		super(name, yearOfBirth);
 	}
--- model/Role.java
+++ model/Role.java
@@ -2,4 +2,7 @@ package application.model;
 
 public class Role {
 
+	private double ProfitPerHour;
+	private String jobTitle;
+
 }

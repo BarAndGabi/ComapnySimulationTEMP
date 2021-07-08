32a283a45aebc52f91b70e480f9ab85b56ce1fc0
barmoshe <1barmoshe1@gmail.com>
2021-07-08 16:46:37 +0300

    Update Company.java

--- model/Company.java
+++ model/Company.java
@@ -5,17 +5,33 @@ import java.util.ArrayList;
 public class Company implements CompanyInterface {
 	private ArrayList<Department> departments;
 	private ArrayList<Simulation> SimulationsArchive;
-	private ArrayList<Role> roles;
 
 	public Company() {
 		this.SimulationsArchive = new ArrayList<Simulation>();
 		this.departments = new ArrayList<Department>();
-		this.roles = new ArrayList<Role>();
 		this.addHardCoded();
 	}
 
 	private void addHardCoded() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void addEmployee(Employee a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addDepartment(Department d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addRole(Role r) {
+		// TODO Auto-generated method stub
+		
 	}
 }
